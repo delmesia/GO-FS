7d47a92a1ae87dbddc10a501e7d0bcfd144cad62
delmesia <wendellsmesia@gmail.com>
2024-07-19 02:56:36 +0800

    fixed some configuration

---
+++ sb/cmd/web/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("ui/static/"))
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that starts with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
+}
