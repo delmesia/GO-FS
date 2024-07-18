50fb43158e0898059a43b2e6eff53eb57e9dbcc0
delmesia <wendellsmesia@gmail.com>
2024-07-18 19:20:13 +0800

    initial commit

--- main.go
+++ main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-
 	w.Write([]byte("Hello, from Snippetbox"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet...."))
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil && id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Displaying snippet ID: %d\n", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet...."))
 }
 
